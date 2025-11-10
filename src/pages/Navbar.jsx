e57f291d7cc60cef65ca96648c8aebed858be883
Ratul Fuad <rtlkakakaku@gmail.com>
2025-11-10 15:59:18 +0600

    complete footer item

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
               
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">FinEase</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
@@ -45,7 +45,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn">Login</a>
         </div>
       </div>
     );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../components/Home";
+import HomePage from "../pages/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    children:[
+        {
+            index: true,
+            element: <HomePage></HomePage>
+        }
+    ]
   },
 ]);
 
